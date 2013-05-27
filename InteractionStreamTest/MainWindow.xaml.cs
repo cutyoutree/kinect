edd5fc60188d0c7c4f055c726c9d465c3bdd145f
zouhair <zouhair@debian.(none)>
2013-05-27 16:37:01 +0200

    first kinect commit

---
+++ InteractionStreamTest/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.Interaction;
+
+namespace InteractionStreamTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _sensor;  //The Kinect Sensor the application will use
+        private InteractionStream _interactionStream;
+
+        private Skeleton[] _skeletons; //the skeletons 
+        private UserInfo[] _userInfos; //the information about the interactive users
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            if (DesignerProperties.GetIsInDesignMode(this))
+                return;
+
+            // this is just a test, so it only works with one Kinect, and quits if that is not available.
+            _sensor = KinectSensor.KinectSensors.FirstOrDefault();
+            if (_sensor == null)
+            {
+                MessageBox.Show("No Kinect Sensor detected!");
+                Close();
+                return;
+            }
+
+            _skeletons = new Skeleton[_sensor.SkeletonStream.FrameSkeletonArrayLength];
+            _userInfos = new UserInfo[InteractionFrame.UserInfoArrayLength];
+
+
+            _sensor.DepthStream.Range = DepthRange.Near;
+            _sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+            _sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+            _sensor.SkeletonStream.EnableTrackingInNearRange = true;
+            _sensor.SkeletonStream.Enable();
+
+            _interactionStream = new InteractionStream(_sensor, new DummyInteractionClient());
+            _interactionStream.InteractionFrameReady += InteractionStreamOnInteractionFrameReady;
+
+            _sensor.DepthFrameReady += SensorOnDepthFrameReady;
+            _sensor.SkeletonFrameReady += SensorOnSkeletonFrameReady;
+
+            _sensor.Start();
+        }
+
+
+
+        private void SensorOnSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs skeletonFrameReadyEventArgs)
+        {
+            using (SkeletonFrame skeletonFrame = skeletonFrameReadyEventArgs.OpenSkeletonFrame())
+            {
+                if (skeletonFrame == null)
+                    return;
+
+                try
+                {
+                    skeletonFrame.CopySkeletonDataTo(_skeletons);
+                    var accelerometerReading = _sensor.AccelerometerGetCurrentReading();
+                    _interactionStream.ProcessSkeleton(_skeletons, accelerometerReading, skeletonFrame.Timestamp);
+                }
+                catch (InvalidOperationException)
+                {
+                    // SkeletonFrame functions may throw when the sensor gets
+                    // into a bad state.  Ignore the frame in that case.
+                }
+            }
+        }
+
+        private void SensorOnDepthFrameReady(object sender, DepthImageFrameReadyEventArgs depthImageFrameReadyEventArgs)
+        {
+            using (DepthImageFrame depthFrame = depthImageFrameReadyEventArgs.OpenDepthImageFrame())
+            {
+                if (depthFrame == null)
+                    return;
+
+                try
+                {
+                    _interactionStream.ProcessDepth(depthFrame.GetRawPixelData(), depthFrame.Timestamp);
+                }
+                catch (InvalidOperationException)
+                {
+                    // DepthFrame functions may throw when the sensor gets
+                    // into a bad state.  Ignore the frame in that case.
+                }
+            }
+        }
+
+        private Dictionary<int, InteractionHandEventType> _lastLeftHandEvents = new Dictionary<int, InteractionHandEventType>();
+        private Dictionary<int, InteractionHandEventType> _lastRightHandEvents = new Dictionary<int, InteractionHandEventType>();
+
+        private void InteractionStreamOnInteractionFrameReady(object sender, InteractionFrameReadyEventArgs args)
+        {
+            using (var iaf = args.OpenInteractionFrame()) //dispose as soon as possible
+            {
+                if (iaf == null)
+                    return;
+
+                iaf.CopyInteractionDataTo(_userInfos);
+            }
+
+            StringBuilder dump = new StringBuilder();
+
+            var hasUser = false;
+            foreach (var userInfo in _userInfos)
+            {
+                var userID = userInfo.SkeletonTrackingId;
+                if (userID == 0)
+                    continue;
+
+                hasUser = true;
+                dump.AppendLine("User ID = " + userID);
+                dump.AppendLine("  Hands: ");
+                var hands = userInfo.HandPointers;
+                if (hands.Count == 0)
+                    dump.AppendLine("    No hands");
+                else
+                {
+                    foreach (var hand in hands)
+                    {
+                        var lastHandEvents = hand.HandType == InteractionHandType.Left
+                                                 ? _lastLeftHandEvents
+                                                 : _lastRightHandEvents;
+
+                        if (hand.HandEventType != InteractionHandEventType.None)
+                            lastHandEvents[userID] = hand.HandEventType;
+
+                        var lastHandEvent = lastHandEvents.ContainsKey(userID)
+                                                ? lastHandEvents[userID]
+                                                : InteractionHandEventType.None;
+
+                        dump.AppendLine();
+                        dump.AppendLine("    HandType: " + hand.HandType);
+                        dump.AppendLine("    HandEventType: " + hand.HandEventType);
+                        dump.AppendLine("    LastHandEventType: " + lastHandEvent);
+                        dump.AppendLine("    IsActive: " + hand.IsActive);
+                        dump.AppendLine("    IsPrimaryForUser: " + hand.IsPrimaryForUser);
+                        dump.AppendLine("    IsInteractive: " + hand.IsInteractive);
+                        dump.AppendLine("    PressExtent: " + hand.PressExtent.ToString("N3"));
+                        dump.AppendLine("    IsPressed: " + hand.IsPressed);
+                        dump.AppendLine("    IsTracked: " + hand.IsTracked);
+                        dump.AppendLine("    X: " + hand.X.ToString("N3"));
+                        dump.AppendLine("    Y: " + hand.Y.ToString("N3"));
+                        dump.AppendLine("    RawX: " + hand.RawX.ToString("N3"));
+                        dump.AppendLine("    RawY: " + hand.RawY.ToString("N3"));
+                        dump.AppendLine("    RawZ: " + hand.RawZ.ToString("N3"));
+                    }
+                }
+
+                tb.Text = dump.ToString();
+            }
+
+            if (!hasUser)
+                tb.Text = "No user detected.";
+        }
+    }
+}
